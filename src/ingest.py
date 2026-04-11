2b693b28083e4bec8cf2546c23a8295028b50329
Panagiotis Tsompanoglou <panostsomp0211@gmail.com>
2026-04-11 11:01:25 +0300

    Added a simple text parser

--- src/ingest.py
+++ src/ingest.py
@@ -1,1 +1,12 @@
 #Takes a path to a PDF file and returns the raw text content.
+import pymupdf
+
+def extract_text(pdf_path: str) -> str:
+    doc = pymupdf.open(pdf_path)
+    full_text=[]
+
+    for page in doc:
+        text = page.get_text()
+        full_text.append(text)
+
+    return "\f".join(full_text)
